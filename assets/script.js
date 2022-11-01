41725a738ed9e5ed3f7b9539cc808b169264403c
Katrina Baker <katrinamourine@gmail.com>
2022-10-31 21:07:07 -0700

    adding local storage debug feature

--- assets/script.js
+++ assets/script.js
@@ -48,16 +48,16 @@ saveButton.on("click", function () {
     localStorage.setItem(timeSlot, userPlan);
 });
 
-//function to save plans across page reloads NOT FUNCTIONING==========================================================================
+//save plans across page reloads======================================================================================================
 $("#9 .description").val(localStorage.getItem("9"));
-$("#9 .description").val(localStorage.getItem("9"));
-$("#9 .description").val(localStorage.getItem("9"));
-$("#9 .description").val(localStorage.getItem("9"));
-$("#9 .description").val(localStorage.getItem("9"));
-$("#9 .description").val(localStorage.getItem("9"));
-$("#9 .description").val(localStorage.getItem("9"));
-$("#9 .description").val(localStorage.getItem("9"));
-$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("12 .description").val(localStorage.getItem("12"));
+$("#1 .description").val(localStorage.getItem("1"));
+$("#2 .description").val(localStorage.getItem("2"));
+$("#3 .description").val(localStorage.getItem("3"));
+$("#4 .description").val(localStorage.getItem("4"));
+$("#5 .description").val(localStorage.getItem("5"));
 
 //Calling functions =================================================================================================================
 timeOfDay();
